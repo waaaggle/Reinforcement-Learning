01fd9b69a9aab42193ebac7be2aa127c31692f0b
waaaggle <143236086+waaaggle@users.noreply.github.com>
2025-07-04 15:28:29 +0800

    Add files via upload

--- utils/parse_config.py
+++ utils/parse_config.py
@@ -1,7 +1,7 @@
 import os
 import configparser
 
-def load_training_config():
+def load_training_config(param_section):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     config_path = os.path.join(current_dir, 'config.ini')
 
@@ -10,26 +10,35 @@ def load_training_config():
 
     # 解析 TRAINING 部分的参数并根据需要转型
     training_params = {}
-    training_params['sample_pool_size'] = config.getint('TRAINING', 'sample_pool_size')
-    training_params['num_episodes'] = config.getint('TRAINING', 'num_episodes')
-    training_params['num_episodes_per_train'] = config.getint('TRAINING', 'num_episodes_per_train')
-    training_params['batch_size'] = config.getint('TRAINING', 'batch_size')
-    training_params['hidden_dim'] = config.getint('TRAINING', 'hidden_dim')
-    training_params['action_bound'] = config.getint('TRAINING', 'action_bound')
-    training_params['learning_rate'] = config.getfloat('TRAINING', 'learning_rate')
-    training_params['gamma'] = config.getfloat('TRAINING', 'gamma')
-    training_params['epsilon'] = config.getfloat('TRAINING', 'epsilon')
-    training_params['tau'] = config.getfloat('TRAINING', 'tau')
-    training_params['sigma'] = config.getfloat('TRAINING', 'sigma')
-    training_params['noise_std'] = config.getfloat('TRAINING', 'noise_std')
+    if param_section == 'TRAINING':
+        training_params['sample_pool_size'] = config.getint(param_section, 'sample_pool_size')
+        training_params['num_episodes'] = config.getint(param_section, 'num_episodes')
+        training_params['num_episodes_per_train'] = config.getint(param_section, 'num_episodes_per_train')
+        training_params['batch_size'] = config.getint(param_section, 'batch_size')
+    elif param_section == 'DDQN':
+        training_params['hidden_dim'] = config.getint(param_section, 'hidden_dim')
+        training_params['learning_rate'] = config.getfloat(param_section, 'learning_rate')
+        training_params['gamma'] = config.getfloat(param_section, 'gamma')
+        training_params['epsilon'] = config.getfloat(param_section, 'epsilon')
+        training_params['tau'] = config.getfloat(param_section, 'tau')
+    elif param_section == 'DDPG':
+        training_params['hidden_dim'] = config.getint(param_section, 'hidden_dim')
+        training_params['action_bound'] = config.getint(param_section, 'action_bound')
+        training_params['learning_rate'] = config.getfloat(param_section, 'learning_rate')
+        training_params['gamma'] = config.getfloat(param_section, 'gamma')
+        training_params['tau'] = config.getfloat(param_section, 'tau')
+        training_params['noise_std'] = config.getfloat(param_section, 'noise_std')
+    elif param_section == 'PPO':
+        pass
+    else:
+        raise Exception('invalid param type:{}'.format(param_section))
 
     return training_params
 
 # 使用示例
 if __name__ == '__main__':
-    params = load_training_config()
+    params = load_training_config('TRAINING')
     print('训练参数:', params)
     # 例如用法
-    learning_rate = params['learning_rate']
     batch_size = params['batch_size']
     # ... 其它参数
