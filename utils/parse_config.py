88210b31a21c616a8ea59ecf7bfac781bb045779
waaaggle <143236086+waaaggle@users.noreply.github.com>
2025-07-11 17:14:32 +0800

    Add files via upload

--- utils/parse_config.py
+++ utils/parse_config.py
@@ -13,23 +13,34 @@ def load_training_config(param_section):
     if param_section == 'TRAINING':
         training_params['sample_pool_size'] = config.getint(param_section, 'sample_pool_size')
         training_params['num_episodes'] = config.getint(param_section, 'num_episodes')
+        training_params['warmup_steps'] = config.getint(param_section, 'warmup_steps')
         training_params['num_episodes_per_train'] = config.getint(param_section, 'num_episodes_per_train')
         training_params['batch_size'] = config.getint(param_section, 'batch_size')
     elif param_section == 'DDQN':
+        training_params['epsilon_decay_steps'] = config.getint(param_section, 'epsilon_decay_steps')
         training_params['hidden_dim'] = config.getint(param_section, 'hidden_dim')
         training_params['learning_rate'] = config.getfloat(param_section, 'learning_rate')
         training_params['gamma'] = config.getfloat(param_section, 'gamma')
-        training_params['epsilon'] = config.getfloat(param_section, 'epsilon')
+        training_params['epsilon_start'] = config.getfloat(param_section, 'epsilon_start')
+        training_params['epsilon_end'] = config.getfloat(param_section, 'epsilon_end')
         training_params['tau'] = config.getfloat(param_section, 'tau')
     elif param_section == 'DDPG':
         training_params['hidden_dim'] = config.getint(param_section, 'hidden_dim')
         training_params['action_bound'] = config.getint(param_section, 'action_bound')
-        training_params['learning_rate'] = config.getfloat(param_section, 'learning_rate')
+        training_params['actor_learning_rate'] = config.getfloat(param_section, 'actor_learning_rate')
+        training_params['critic_learning_rate'] = config.getfloat(param_section, 'critic_learning_rate')
         training_params['gamma'] = config.getfloat(param_section, 'gamma')
         training_params['tau'] = config.getfloat(param_section, 'tau')
         training_params['noise_std'] = config.getfloat(param_section, 'noise_std')
     elif param_section == 'PPO':
-        pass
+        training_params['hidden_dim'] = config.getint(param_section, 'hidden_dim')
+        training_params['action_bound'] = config.getint(param_section, 'action_bound')
+        training_params['epochs'] = config.getint(param_section, 'epochs')
+        training_params['actor_learning_rate'] = config.getfloat(param_section, 'actor_learning_rate')
+        training_params['critic_learning_rate'] = config.getfloat(param_section, 'critic_learning_rate')
+        training_params['eps'] = config.getfloat(param_section, 'eps')
+        training_params['gamma'] = config.getfloat(param_section, 'gamma')
+        training_params['lamda'] = config.getfloat(param_section, 'lamda')
     else:
         raise Exception('invalid param type:{}'.format(param_section))
 
